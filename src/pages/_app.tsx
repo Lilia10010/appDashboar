11739b7e69cf9f8c0690b48b59e84e8237342148
Lília Paula <liliaxtz2@gmail.com>
2022-04-27 19:27:19 -0300

    feat: add mirage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,15 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { SidebarDrawerProvider } from '../context/SidebarDraweContext'
+import { makeServer } from '../../services/mirage'
+
+//para inicializar o servidor do mirage
+//verificar se está rodando em ambiente de desenvolvimento
+//(variável setada de forma automatica pelo next)
+
+if(process.env.NODE_ENV === 'development'){
+  makeServer()
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
